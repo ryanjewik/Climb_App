d29dbf306b4c186658cd50c6418ec61ee079011f
Szymon Kozlowski <92408910+szykozlowski@users.noreply.github.com>
2023-11-14 19:46:40 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flaskext.mysql import MySQL
+from sqlalchemy import create_engine
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask"
